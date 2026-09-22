76f750ed78d1340d3f85f0d6c120a1439cf9ffbc
jeffersonagm <tinalmeida_2000@hotmail.com>
2026-09-22 14:15:39 -0500

    Delete exercise2.py

--- exercise2.py
+++
@@ -1,9 +0,0 @@
-productos = [
-    {"item": "Pan", "price": 1.50},
-    {"item": "Leche", "price": 2.00},
-    {"item": "Arroz", "price": 3.00}
-]
-total = 0
-for producto in productos:
-    total += producto["price"]
-print("El total de la compra es: $", total)
